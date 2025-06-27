36b13c0264064285e6d33a1d17002389b01b913e
lcx <chengxi.luo2004@gmail.com>
2025-06-27 01:05:51 -0400

    feat: Add WORLD

--- foreign/go/cli/cli.go
+++ foreign/go/cli/cli.go
@@ -83,6 +83,7 @@ var (
 
 func getUsage() {
 	fmt.Println("HELLO")
+	fmt.Println("World")
 	fmt.Println("Usage:")
 	fmt.Println("  getstream    -url <url> -port <port> -streamId <streamId>")
 	fmt.Println("  createstream -url <url> -port <port> -streamId <streamId> -name <name>")
