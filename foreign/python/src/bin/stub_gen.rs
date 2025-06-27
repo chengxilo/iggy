b43559781e5ecce9e630118c2dfe8b3f5fa802ee
lcx <chengxi.luo2004@gmail.com>
2025-06-27 01:15:45 -0400

    feat: print main in python

--- foreign/python/src/bin/stub_gen.rs
+++ foreign/python/src/bin/stub_gen.rs
@@ -20,6 +20,7 @@ use pyo3_stub_gen::Result;
 
 fn main() -> Result<()> {
     print("main")
+    print("hello")
     // `stub_info` is a function defined by `define_stub_info_gatherer!` macro.
     let stub = iggy_py::client::stub_info()?;
     stub.generate()?;
